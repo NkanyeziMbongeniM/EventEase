d8311ce0ddb0143ddf1f0e4c238db27692ce9d0d
NkanyeziMbongeniM <lisyberry@gmail.com>
2026-06-11 00:15:16 +0200

    Fix duplicate venue properties

--- Models/Venue.cs
+++ Models/Venue.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace EventEase.Models
@@ -7,30 +6,19 @@ namespace EventEase.Models
     {
         public int VenueID { get; set; }
 
-        [Required(ErrorMessage = "Venue name is required")]
-        [StringLength(100)]
-        public string? VenueName { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Location is required")]
-        [StringLength(100)]
-        public string? Location { get; set; } = string.Empty;
-
         [Required]
-        [StringLength(100)]
         public string VenueName { get; set; } = string.Empty;
 
         [Required]
-        [StringLength(100)]
         public string Location { get; set; } = string.Empty;
 
-        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than 0.")]
         public int Capacity { get; set; }
 
         public string? ImageUrl { get; set; }
 
         public bool IsAvailable { get; set; } = true;
 
-        public ICollection<Event> Events { get; set; } = new List<Event>();
-        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+        public ICollection<Event>? Events { get; set; }
+        public ICollection<Booking>? Bookings { get; set; }
     }
 }
